3ca08e84f4b9321ad38d8d305f232f9767292e8c
amelia v <amelia@ag2v.com>
2025-06-18 15:57:20 -0400

    submodule

---
+++ src/whale_eeprom.c
@@ -0,0 +1,12 @@
+#include "rp2x_cat24m01.h"
+#include "whale_eeprom.h"
+
+int w_eeprom_init(void) {
+	
+}
+
+
+int w_eeprom_full_erase(void);
+
+int w_eeprom_write(uint16_t page, uint8_t addr, uint8_t src, int size);
+int w_eeprom_read(uint16_t page, uint8_t addr, uint8_t *dst, int size);
