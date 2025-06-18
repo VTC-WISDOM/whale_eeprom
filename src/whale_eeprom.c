2e2c53ca37e1933d5cda1ceba4e3387570b9e84c
amelia v <amelia@ag2v.com>
2025-06-18 16:26:24 -0400

    byte write/read

--- src/whale_eeprom.c
+++ src/whale_eeprom.c
@@ -8,5 +8,5 @@ int w_eeprom_init(void) {
 
 int w_eeprom_full_erase(void);
 
-int w_eeprom_write(uint16_t page, uint8_t addr, uint8_t src, int size);
-int w_eeprom_read(uint16_t page, uint8_t addr, uint8_t *dst, int size);
+int w_eeprom_write(int page, uint8_t addr, uint8_t *src, int size);
+int w_eeprom_read(int page, uint8_t addr, uint8_t *dst, int size);
